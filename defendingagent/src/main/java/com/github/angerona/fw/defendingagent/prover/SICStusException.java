94c3ee8ba32d859fda89f677d21946a6fcc80092
Manuel Barbi <manuel.barbi@tu-dortmund.de>
2015-12-18 11:46:29 +0100

    depending agent back to normal

--- defendingagent/src/main/java/com/github/angerona/fw/defendingagent/prover/SICStusException.java
+++ defendingagent/src/main/java/com/github/angerona/fw/defendingagent/prover/SICStusException.java
@@ -1,6 +1,8 @@
-package com.github.angerona.fw.defendingagent.Prover;
+package com.github.angerona.fw.defendingagent.prover;
 
 public class SICStusException extends Exception {
+
+	private static final long serialVersionUID = -8674557467204996643L;
 
 	public SICStusException(String string) {
 		super(string);
